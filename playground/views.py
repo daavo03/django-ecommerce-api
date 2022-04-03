50ffc7d2f0e9567648a38edbbf619a99ba015488
Danielvo <daavo.1002@gmail.com>
2022-04-03 17:40:34 -0500

    Retrieving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 def say_hello(request):
   # Every model in django has an attribute called ".objects", this return a manager object (interface to DB)
   #Most of the methods return a queryset (object that encapsulate a query). At some point django will evaluate the
   #query and django will generate the right SQL statement to sent to our DB. This will happen under a few scenarios
-  query_set = Product.objects.all()
+  #query_set = Product.objects.all()
 
   # One scenario when we iterate over a queryset
   #for product in query_set:
@@ -22,6 +22,26 @@ def say_hello(request):
   # To filter the results, will return a new query set, we can chain it to call a 2nd filter method, also use the order_by
   #method to sort the result
   #query_set.filter().filter().order_by()
+
+  # Methods for retrieving objects
+  #all()  When it's evaluated we get all the objects in a given table
+  #queryset = Product.objects.all()
+  #get()     Getting a single object
+  #product = Product.objects.get(id=1) #pk=1 when using pk django will auto translate this to the name of the PK field
+  #if it cannot find this object it will throw an exception, to handle this wrap code inside try/catch
+  try:
+    product = Product.objects.get(pk=0)
+  except ObjectDoesNotExist:
+    pass
+
+  #Better way to try-catch
+  #The filter() method returns a queryset, so rightaway we can call the first() method of the queryset
+  #if the queryset is empty the first() returns none. So in this case "product" is going to be none
+  product2 = Product.objects.filter(pk=0).first()
+
+  #Check the existence of an object
+  #We get a boolean value
+  exists = Product.objects.filter(pk=0).exists()
 
 
   return render(request, 'hello.html', { 'name': 'Daniel' })

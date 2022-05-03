19a9fa6541a67a013a4fef31fa1d160b66113683
Danielvo <daavo.1002@gmail.com>
2022-05-03 18:55:48 -0500

    Using Generic Relations

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+# Register our Tag model so we can manage our tags in the admin interface
+@admin.register(Tag)
+# Creating an admin class for our Tag model
+class TagAdmin(admin.ModelAdmin):
+  search_fields = ['label']

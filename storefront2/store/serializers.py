bfefe1df7d10bcf67e571b2f67ccd6f2421b7f20
Danielvo <daavo.1002@gmail.com>
2022-05-04 01:41:28 -0500

    Model Serializers

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -6,36 +6,47 @@ from store.models import Product, Collection
 
 
 # Including a Nested Object. First we need to create a class
-class CollectionSerializer(serializers.Serializer):
-  id = serializers.IntegerField()
-  title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+  #Applying the same changes with the ModelSerializer
+  class Meta:
+    model = Collection
+    fields = ['id', 'title']
 
-class ProductSerializer(serializers.Serializer):
-  # Deciding what fields of the Product class we wanna serialize
-  #Defining the fields as we would define them in the model
-  id = serializers.IntegerField()
-  #Later we use this serializer when receiving data
-  title = serializers.CharField(max_length=255)
-  #We can name them whatever, it's completely separate object from the Product object with argument "source"
-  price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-  # New field defining a new method which will return a value for this field
+# Using ModelSerializer to quickly create a serializer. To a related field by default it use PK related field 
+class ProductSerializer(serializers.ModelSerializer):
+  # Creating a meta class
+  class Meta:
+    model = Product
+    # An array or tuple of the fields to include
+    #Including a field does not exist, we add it and define it below
+    fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
   price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-  # Including relating objects like "collection" with this we can include the PK or the ID of each collection in
-  #a product object
-  #collection = serializers.PrimaryKeyRelatedField(
-    # Setting the argument for looking collections
-    #queryset = Collection.objects.all()
-  #)
-  #Another way is to return a collection as a string, returning the name of each collection
-  #collection = serializers.StringRelatedField()
-  # Including a nested object
-  #collection = CollectionSerializer()
-  # Including a link to an endpoint for viewing that Collection
-  collection = serializers.HyperlinkedRelatedField(
-    queryset = Collection.objects.all(),
-    # This argument is used for generating hyperlink
-    view_name='collection-detail'
-  )
+
+  # # Deciding what fields of the Product class we wanna serialize
+  # #Defining the fields as we would define them in the model
+  # id = serializers.IntegerField()
+  # #Later we use this serializer when receiving data
+  # title = serializers.CharField(max_length=255)
+  # #We can name them whatever, it's completely separate object from the Product object with argument "source"
+  # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+  # # New field defining a new method which will return a value for this field
+  # # Including relating objects like "collection" with this we can include the PK or the ID of each collection in
+  # #a product object
+  # #collection = serializers.PrimaryKeyRelatedField(
+  #   # Setting the argument for looking collections
+  #   #queryset = Collection.objects.all()
+  # #)
+  # #Another way is to return a collection as a string, returning the name of each collection
+  # #collection = serializers.StringRelatedField()
+  # # Including a nested object
+  # #collection = CollectionSerializer()
+  # # Including a link to an endpoint for viewing that Collection
+  # collection = serializers.HyperlinkedRelatedField(
+  #   queryset = Collection.objects.all(),
+  #   # This argument is used for generating hyperlink
+  #   view_name='collection-detail'
+  # )
 
 
   #Defining the method

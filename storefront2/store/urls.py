918dfa123de026c0f036bc6e97668c58eb7aafca
Danielvo <daavo.1002@gmail.com>
2022-05-05 19:51:03 -0500

    Routers

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -1,8 +1,24 @@
 # Mapping the view function to URL pattern
 
 from django.urls import path
+from django.urls.conf import include
+# We also have another router DefaultRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
 
+
+
+
+# Creating router object
+# If we use DeafultRouter we get 2 additional features. 1) basic root view in localhost/store, 2) getting data in json adding .json of route
+#router = SimpleRouter()
+router = DefaultRouter()
+# Register our viewsets with this router object. We'll be saying that the products endpoint should be manage by the ProductViewSet 
+#Passing 2 arguments: 1. Prefix value we're using as the name of our endpoint "products", 2. Our viewset
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+"""
 # URLConf
 urlpatterns = [
     # In order to use view class need to use as_view() method which will convert the class to a regular function based view
@@ -16,3 +32,15 @@ urlpatterns = [
     # also changing the parameter to pk
     path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
 ]
+"""
+
+# URLConf
+# If we don't have explicit patterns
+urlpatterns = router.urls
+""" 
+# If we have some specific patterns in the array
+urlpatterns = [
+    # For route it's an empty string, and we're gonna include() and with it we can import routes from somewhere else
+    path('', include(router.urls))
+] 
+"""
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Collection, Product
+from .models import Collection, OrderItem, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
 """ 
@@ -106,12 +106,14 @@ class ProductViewSet(ModelViewSet):
   def get_serializer_context(self):
       return {'request': self.request}
 
-  def delete(self, request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if product.orderitems.count() > 0:
+  # We need to overwrite the destroy() method, using this new class in the ViewSet
+  def destroy(self, request, *args, **kwargs):
+    # Changing the validation logic to not retrieve get_object twice
+    #From the kwargs argument we can read the pk argument
+    if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
       return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    product.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    return super().destroy(request, *args, **kwargs)
 
 """ 
 # Updating a product
@@ -207,13 +209,12 @@ class CollectionList(ListCreateAPIView):
 class CollectionViewSet(ModelViewSet):
   queryset = Collection.objects.annotate(products_count=Count('products')).all()
   serializer_class = CollectionSerializer
-  
-  def delete(self, request, pk):
-    collection = get_object_or_404(Collection, pk=pk)
-    if collection.products.count() > 0:
+
+  def destroy(self, request, *args, **kwargs):
+    if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
       return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    collection.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+
+    return super().destroy(request, *args, **kwargs)
 
 """
 # Get-Update-Delete a Collection

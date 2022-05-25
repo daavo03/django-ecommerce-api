6aae2e3d989ccbc30d23aa9f00b7b1fe9dacb48d
Danielvo <daavo.1002@gmail.com>
2022-05-25 17:10:16 -0500

    Managin Media Files

--- storefront3/storefront/settings.py
+++ storefront3/storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -138,6 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Setting for the endpoint at which we'll expose our user uploaded files
+MEDIA_URL = '/media/'
+# Telling Django where this media files are stored in the FS
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

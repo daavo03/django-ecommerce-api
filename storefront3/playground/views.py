d2790aad025c9a2a981af87e7b00ac9870893320
Danielvo <daavo.1002@gmail.com>
2022-05-26 19:07:37 -0500

    Attaching Files to Emails

--- storefront3/playground/views.py
+++ storefront3/playground/views.py
@@ -1,13 +1,15 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
     # Sending an email
     try:
-        # Sending emails to admins
-        mail_admins('subject', 'message', html_message='message')
-        # send_mail('subject', 'message', 'info@daavo.com', ['bren@daavo.com'])
+        # Creating EmailMessage object
+        message = EmailMessage('subject', 'message', 'from@daavo.com', ['bren@daavo.com'])
+        # Give the method the path relative to our project directory
+        message.attach_file('playground/static/images/dog.jpeg')
+        message.send()
     except BadHeaderError:
         # Returning an error to the client in this case we pass
         pass
